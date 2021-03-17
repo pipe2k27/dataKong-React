1affbc7f0e13975fce5f123ae22d09067292f1c4
Felipe Perarnau <59627664+pipe2k27@users.noreply.github.com>
2021-03-17 17:14:57 -0300

    change a link

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -15,7 +15,7 @@ export default function Footer() {
       ></img>
       <p>info@aidafy.com</p>
       <p>®Aidafy</p>
-      <a className='wallLink' href='https://requests.datawallet.com?cid=12e7b5ac-aec2-4d31-b16c-af6862594aa9'>Data Subject Request (Claim your data)</a>
+      <a className='wallLink' href='https://stg.privacy-saas.wibson.io/#/data-request/user/c0fd2283-43f2-4a1d-a784-55c33627bc74'>Data Subject Request (Claim your data)</a>
       <a className='wallLink' href='https://requests.datawallet.com/sale?cid=12e7b5ac-aec2-4d31-b16c-af6862594aa9'>Do not sell my data</a>
     </div>
   );
